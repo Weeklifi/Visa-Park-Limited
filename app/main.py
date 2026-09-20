97d32eabdf0469e4ce5e3c3a15249ec268f3089e
Rafid Bin Bakhtiar <rafidbinbakhtiar2002@gmail.com>
2026-09-20 15:08:50 +0600

    cors middleware

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Multi-Tier Pyramid Marketplace Engine", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://187.77.132.222:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
